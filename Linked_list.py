7c865c7d51ad5df2f0be8a70a2614e3da0c5817f
arshadshk246 <103821414+arshadshk246@users.noreply.github.com>
2022-09-26 19:20:25 +0530

    Update Linked_list.py

--- Linked_list.py
+++ Linked_list.py
@@ -14,13 +14,13 @@ class LinkedList:
             temp = temp.next
 
 lst = LinkedList()
-lst.head = Node(1)
-second = Node(2)
-third = Node(3)
+lst.head = Node(1)  # Add the head node
+second = Node(2)    # Add the second node
+third = Node(3)     # Add the second node
 
-lst.head.next = second
-second.next = third
+lst.head.next = second  # link the head node to the second node
+second.next = third     # link the second node to the third node
 
-lst.printList() 
+lst.printList() # Print the linked list
 
 
